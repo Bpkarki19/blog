b4d70798d07ec78899538ad5d3e1a989ff348185
Bpkarki19 <bipinkarki9861@gmail.com>
2025-12-27 13:56:20 +0900

    improving tag

--- src/components/PopularTags.jsx
+++ src/components/PopularTags.jsx
@@ -1,10 +1,11 @@
 import Tag from '../components/UI/Tag'
-export default function PopularTags(){
-    return(
-        <div className='w-[800px] border-1 border-gray-200 rounded-md p-1 shadow-sm'>
+export default function PopularTags() {
+    return (
+        <div className='w-[800px] border-1 border-gray-200 rounded-md p-1 shadow-sm px-5 pb-[16px]'>
             <h1>Popular tags</h1>
-            <div>
-                <Tag>react</Tag>
+            <div className='flex flex-wrap gap-2'>
+                <Tag clickable>react</Tag>
+                <Tag clickable={true}>Next.js</Tag>
             </div>
 
         </div>
