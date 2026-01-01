2599dcadaa9977781b2da08aa507c3ee58a56318
Bpkarki19 <bipinkarki9861@gmail.com>
2026-01-01 10:58:40 +0900

    Fetching tags

--- src/components/PopularTags.jsx
+++ src/components/PopularTags.jsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react';
 import Tag from '../components/UI/Tag'
+import api from '../services/api'
 export default function PopularTags() {
+    const [tags, setTags] = useState([]);
+    
+    useEffect(()=>{
+        const getData = async ()=>{
+            try{
+            const response = await api.get('/tags');
+            console.log(response.data.tags);
+            setTags(response.data.tags);
+        }catch(errors){
+            console.error(errors);
+        }
+
+        };
+        getData();
+
+        
+    },[]);//run once
+    
     return (
         <div className='max-w-7xl mx-auto border border-[#AAAAAA] rounded-md p-1 px-5 pb-[16px]'>
             <h1 className='pb-2'>Popular tags</h1>
             <div className='flex flex-wrap gap-2'>
-                <Tag clickable>react</Tag>
-                <Tag clickable={true}>Next.js</Tag>
+                {tags.slice(0,10).map(tag=>
+                    <Tag key={tag} clickable>{tag}</Tag>
+                )}
             </div>
 
         </div>
--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
 });
 
 // Automatically add Token to every request if it exists
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((config) => {//Every time sending request this func stops and checks local storage for token and stamp the request with security badge
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Token ${token}`;
